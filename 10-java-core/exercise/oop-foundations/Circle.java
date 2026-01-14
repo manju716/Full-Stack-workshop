18ff3289d1bd8bdfb1e5e4ea99c56ef09cea695a
manju716 <135322588+manju716@users.noreply.github.com>
2026-01-14 09:49:15 +0530

    Update Circle.java

---
+++ 10-java-core/exercise/oop-foundations/Circle.java
@@ -0,0 +1,20 @@
+package Person;
+
+public class Circle extends Shape {
+
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return 3.14 * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * 3.14 * radius;
+    }
+}
