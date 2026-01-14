6e41c263a6ecf8cbc018fee10eaed835a899fb66
manju716 <135322588+manju716@users.noreply.github.com>
2026-01-14 09:51:05 +0530

    Update Shape.java

---
+++ 10-java-core/exercise/oop-foundations/Shape.java
@@ -0,0 +1,15 @@
+package Person;
+
+public abstract class Shape {
+
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    public void displayInfo() {
+        System.out.println("Area: " + area());
+        System.out.println("Perimeter: " + perimeter());
+        System.out.println("--------------------");
+    }
+}
+
