e94ca64dae0f924cc542dcc25fda37c9b2888d4c
manjula <manjulakvkaki86@gmail.com>
2026-01-14 23:52:33 +0530

    challenge3

---
+++ 10-java-core/challenges/challenge3_SafeExecutor.java
@@ -0,0 +1,31 @@
+package stream;
+
+import java.util.Optional;
+
+public class SafeExecutor {
+
+    @FunctionalInterface
+    interface ThrowingSupplier<T> {
+        T get() throws Exception;
+    }
+
+    public static <T> Optional<T> safeExecute(ThrowingSupplier<T> supplier) {
+        try {
+            return Optional.of(supplier.get());
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+ 
+    public static void main(String[] args) {
+
+        System.out.println(
+                safeExecute(() -> Integer.parseInt("123"))
+        ); 
+
+        System.out.println(
+                safeExecute(() -> Integer.parseInt("abc"))
+        ); 
+    }
+}
