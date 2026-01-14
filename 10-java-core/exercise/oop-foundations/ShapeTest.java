7790651fcb539683391f472cca42bd65fe6217b9
manju716 <135322588+manju716@users.noreply.github.com>
2026-01-14 09:51:27 +0530

    Update ShapeTest.java

---
+++ 10-java-core/exercise/oop-foundations/ShapeTest.java
@@ -0,0 +1,17 @@
+package Person;
+
+public class ShapeTest {
+
+    public static void main(String[] args) {
+
+        Shape[] shapes = {
+            new Circle(5),
+            new Rectangle(4, 6),
+            new Triangle(3, 4, 3, 4, 5)
+        };
+
+        for (Shape s : shapes) {
+            s.displayInfo();   
+        }
+    }
+}
